a861e35cf17e05773a8dae80fa0a7889a8f9a627
mohamadAlrimi <mohamadrimi12345@gmail.com>
2024-11-26 23:49:26 +0300

    First Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,45 @@
-import logo from '../assets/logo.png';
+import { useRef } from 'react';
 
-export default function Header() {
+import CartModal from './CartModal.jsx';
+
+export default function Header({ cart, onUpdateCartItemQuantity }) {
+  const modal = useRef();
+
+  const cartQuantity = cart.items.length;
+
+  function handleOpenCartClick() {
+    modal.current.open();
+  }
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
+    <>
+      <CartModal
+        ref={modal}
+        cartItems={cart.items}
+        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+        title="Your Cart"
+        actions={modalActions}
+      />
+      <header id="main-header">
+        <div id="main-title">
+          <img src="logo.png" alt="Elegant model" />
+          <h1>Elegant Context</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
   );
 }
